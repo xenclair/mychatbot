2365f2d885eec6c98c538d21c33fd57d277bcb50
Cindy Adelia Setiawan <cindy.adelia@bukalapak.com>
2023-10-12 16:50:31 +0700

    Add save cookies to local dir

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,11 @@ def generate_response(prompt_input, email, passwd):
     # Hugging Face Login
     sign = Login(email, passwd)
     cookies = sign.login()
-    sign.saveCookies()
+    
+    # Save cookies to the local directory
+	cookie_path_dir = "./cookies_snapshot"
+	sign.saveCookiesToDir(cookie_path_dir)
+    
     # Create ChatBot                        
     chatbot = hugchat.ChatBot(cookies=cookies.get_dict())
     return chatbot.chat(prompt_input)
