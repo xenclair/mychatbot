e9fa03c3a212ebf8fe959ea98680ef86d6b41f20
Cindy Adelia Setiawan <cindy.adelia@bukalapak.com>
2023-10-12 16:57:26 +0700

    Change save cookies function

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,9 +36,7 @@ def generate_response(prompt_input, email, passwd):
     sign = Login(email, passwd)
     cookies = sign.login()
     
-    # Save cookies to the local directory
-    cookie_path_dir = "./cookies_snapshot"
-    sign.saveCookiesToDir(cookie_path_dir)
+    sign.saveCookies()
     
     # Create ChatBot                        
     chatbot = hugchat.ChatBot(cookies=cookies.get_dict())
